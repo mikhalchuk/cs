8e7f1184b7fe5b3353d72cf7282c3e13f61fb5c8
Sergii Mikhalchuk <sergii.mykhalchuk@gmail.com>
2018-02-05 20:52:57 +0100

    move task to a new package and add test

---
+++ hackerrank/stack/maximum_element.go
@@ -0,0 +1,76 @@
+package hackerrank
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"fmt"
+	"stack"
+)
+
+var st = &stack.StackList{}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	var n, i int64
+	fmt.Scanf("%d", &n)
+
+	for i = 0; i < n; i++ {
+		s, _ := reader.ReadString('\n')
+
+		cmd, input := parseCommand(s)
+
+		max, peek := step([2]int64{cmd,input})
+
+		if peek {
+			fmt.Println(max)
+		}
+	}
+}
+
+func step(input [2]int64) (int64, bool) {
+	cmd := input[0]
+	switch cmd {
+	case 1:
+		newVal := input[1]
+		var toPush int64
+		head, err := st.Peek()
+		if err != nil {
+			toPush = newVal
+		} else {
+			if head.(int64) > newVal {
+				toPush = head.(int64)
+			} else {
+				toPush = newVal
+			}
+		}
+		st.Push(toPush)
+	case 2:
+		st.Pop()
+	case 3:
+		val, _ := st.Peek()
+		return val.(int64), true
+	}
+
+	return 0, false
+}
+
+func parseCommand(s string) (cmd, val int64) {
+	s = strings.Trim(s, "\n")
+	args := strings.Split(s, " ")
+	cmd = parseInt(args[0])
+	if len(args) == 1 {
+		return
+	}
+
+	val = parseInt(args[1])
+	return
+}
+
+func parseInt(s string) int64 {
+	num, _ := strconv.ParseInt(s, 10, 64)
+	return num
+}
---
+++ hackerrank/stack/maximum_element_test.go
@@ -0,0 +1,39 @@
+package hackerrank
+
+import (
+	"testing"
+)
+
+func Test_main(t *testing.T)  {
+
+	input := [10][2]int64{}
+	input[0][0] = 1
+	input[0][1] = 97
+	input[1][0] = 2
+	input[2][0] = 1
+	input[2][1] = 20
+	input[3][0] = 2
+	input[4][0] = 1
+	input[4][1] = 26
+	input[5][0] = 1
+	input[5][1] = 20
+	input[6][0] = 2
+	input[7][0] = 3
+	input[8][0] = 1
+	input[8][1] = 91
+	input[9][0] = 3
+
+
+	cnt := 0
+	res := [2]int64{26, 91}
+
+	for _, val := range input {
+		max, _ := step(val)
+		if val[0] == 3 {
+			if max != res[cnt] {
+				t.Errorf("%d != %d step is wrong", res[cnt], max)
+			}
+			cnt++
+		}
+	}
+}
